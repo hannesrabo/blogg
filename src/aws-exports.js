45b8d1c43bdcb3beb8fb8485f95944bfa48d0eca
Hannes Rabo <hannes.rabo@gmail.com>
2018-03-26 12:55:13 +0200

    Added the config file

---
+++ src/aws-exports.js
@@ -0,0 +1,21 @@
+
+// AWS Mobile Hub Project Constants
+const awsmobile = {
+    'aws_app_analytics': 'enable',
+    'aws_cognito_identity_pool_id': 'eu-central-1:e6a24bba-0807-4f54-96dd-c6e040d07567',
+    'aws_cognito_region': 'eu-central-1',
+    'aws_mobile_analytics_app_id': '314ab79a6ee54d8a98d85093b1754cee',
+    'aws_mobile_analytics_app_region': 'us-east-1',
+    'aws_project_id': '9d81ffa7-2d0c-11e8-8692-f56c6f73a3e3',
+    'aws_project_name': 'blogging-react-front-end-2018-03-21-14-34-40',
+    'aws_project_region': 'eu-central-1',
+    'aws_resource_name_prefix': 'bloggingreactfronten-mobilehub-1112617913',
+    'aws_sign_in_enabled': 'enable',
+    'aws_user_pools': 'enable',
+    'aws_user_pools_id': 'eu-central-1_CsJz5MZ5w',
+    'aws_user_pools_mfa_type': 'ON',
+    'aws_user_pools_web_client_id': '42v5obpenork1892q91m81mv7m',
+
+}
+
+export default awsmobile;
